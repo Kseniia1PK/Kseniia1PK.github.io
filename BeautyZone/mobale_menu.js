7a2491770ff9002c71eb4736dc7cbc5f6aee6acc
PKKseniia <podorognikkh@gmail.com>
2023-12-23 17:45:28 +0200

    First section

---
+++ BeautyZone/mobale_menu.js
@@ -0,0 +1,22 @@
+const mobileMenu = document.querySelector('.mobile-menu-container');
+const menuBtnOpen = document.querySelector('.menu-btn-open');
+const menuBtnClose = document.querySelector('.menu-btn-close');
+// const bodyStopScroll = document.querySelector('body');
+// const menuList = document.querySelector('.menu-list');
+
+const toggleMenu = () => mobileMenu.classList.toggle('is-open');
+const toggleBody = () => bodyStopScroll.classList.toggle('lock-scroll');
+
+menuBtnOpen.addEventListener('click',toggleMenu);
+menuBtnClose.addEventListener('click',toggleMenu);
+
+// menuBtnOpen.addEventListener('click',toggleBody);
+// menuBtnClose.addEventListener('click',toggleBody);
+
+
+menuList.addEventListener('click', (event) => {
+  if (event.target.tagName === 'A') {
+    toggleMenu();
+    toggleBody();
+  }
+});
