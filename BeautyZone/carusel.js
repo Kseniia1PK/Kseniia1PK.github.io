52ae6bfa7175806d59c61d80b20ec8b8c28ad3dd
PKKseniia <podorognikkh@gmail.com>
2024-01-01 13:27:09 +0200

    внесення змін у секцію Процедури

--- BeautyZone/carusel.js
+++ BeautyZone/carusel.js
@@ -1,11 +1,15 @@
 const swiper = new Swiper('.swiper-container', {
   loop: true,
-  slidesPerView: 1,
+  slidesPerView: 3,
   spaceBetween: 20,
   breakpoints: {
    
     // when window width is >= 480px
-    650: {
+    360: {
+      slidesPerView: 1,
+    
+    },
+    768: {
       slidesPerView: 2,
     
     },
@@ -19,14 +23,14 @@ const swiper = new Swiper('.swiper-container', {
   // If we need pagination
   pagination: {
     el: '.pagination',
+    clickable: true,
     bulletClass: 'pagination__button',
     bulletActiveClass: 'pagination__button--active',
   },
 
   // Navigation arrows
   navigation: {
-    nextEl: '.carousel-button.next',
-    prevEl: '.carousel-button.prev',
+    nextEl: '.carousel-button.prev',
+    prevEl: '.carousel-button.next',
   },
-
 });
